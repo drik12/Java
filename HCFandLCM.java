a5915bbaebe63264345a36b0293b7eee3ab0f2f8
Diganta Roy Drik <digantaroydrik12@gmail.com>
2026-07-12 18:11:45 +0530

    Day 8

---
+++ HCFandLCM.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class HCFandLCM {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a= sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b= sc.nextInt();
+
+        int num1=a;
+        int num2=b;
+
+        while(b!=0){
+            int temp =b;
+            b=a%b;
+            a = temp;
+
+        }
+
+        int hcf=a;
+
+        int lcm= (num1 * num2)/hcf;
+
+        System.out.println("HCF(GCD) =" + hcf);
+        System.out.println("LCM = " + lcm);
+
+    }
+}
