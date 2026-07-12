633dc7a259b0bc870cb252202b08f363733cf089
Diganta Roy Drik <digantaroydrik12@gmail.com>
2026-07-12 11:02:11 +0530

    Day 7

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -4,21 +4,19 @@ public class ReverseNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter an integer: ");
-        int num = sc.nextInt();
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
 
-        int original = num;
-        int reverse = 0;
-
-        while (num != 0) {
-            int digit = num % 10;
-            reverse = reverse * 10 + digit;
-            num = num / 10;
+        int original=n;
+        int reverse=0;
+        while(n!=0){
+            int digit= n%10;
+            reverse= reverse*10+ digit;
+            n=n/10;
         }
 
-        System.out.println("Original Number: " + original);
-        System.out.println("Reversed Number: " + reverse);
-
-        sc.close();
+        System.out.println("Original num: "+ original);
+        System.out.println("Reversed num: "+ reverse);
     }
+
 }
